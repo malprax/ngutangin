ee1713cb2577fb202c2eeaceb78ca1abd3d349f1
Suhendra <idsuhendra@gmail.com>
2017-05-16 14:02:58 +0700

    add login helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def login_helper style = ''
+    if current_user.present?
+      (link_to "Edit Profile", edit_user_registration_path, class: style) +
+      " ".html_safe +
+      (link_to "Logout", destroy_user_session_path, method: :delete, class: style)
+    else
+      (link_to "Login", new_user_session_path, class: style) +
+      " ".html_safe +
+      (link_to "Sign Up", new_user_registration_path, class: style)
+    end
+  end
 end
