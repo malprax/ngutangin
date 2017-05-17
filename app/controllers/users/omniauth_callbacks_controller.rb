1dff2ac1ab52be36c0892266a194c146427525c4
aulia <kingmalprax@gmail.com>
2017-05-17 10:05:41 +0800

    fix login facebook

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -28,14 +28,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def facebook
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    if @user.persisted?
-      # if session[:booking_search_id].blank?
-        # sign_in_and_redirect @user, :event => :authentication
-        # set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-      # else
-        # sign_in @user
-        # redirect_to new_booking_booking_search_path(session[:booking_search_id], therapist_id: session[:therapist_id])
-      # end
       if @user.persisted?
         sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
         set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
@@ -43,37 +35,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         session["devise.omniauth_data"] = request.env["omniauth.auth"]
         redirect_to new_user_registration_url
       end
-    else
-      session["devise.omniauth_data"] = params_setup(@user)
-      redirect_to new_user_registration_url
-    end
-  end
-
-  def google_oauth2
-    @user = User.from_omniauth(request.env["omniauth.auth"])
-    if @user.persisted?
-      # if session[:booking_search_id].blank?
-        sign_in_and_redirect @user, :event => :authentication
-        set_flash_message(:notice, :success, :kind => "Google") if is_navigational_format?
-      # else
-        # sign_in @user
-        # redirect_to new_booking_booking_search_path(session[:booking_search_id], therapist_id: session[:therapist_id])
-      # end
-    else
-      session["devise.omniauth_data"] = params_setup(@user)
-      redirect_to new_user_registration_url
-    end
   end
 
   def failure
     redirect_to root_path
-  end
-
-  def params_setup(user)
-    {
-      email: user.email,
-      # first_name: user.firstname,
-      # last_name: user.lastname
-    }
   end
 end
