e635880c349834f312068605c609b56ad55efcdd
Suhendra <idsuhendra@gmail.com>
2017-05-18 11:09:04 +0700

    before add friend, cek if friend or not.

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,12 +1,17 @@
 class FriendshipsController < ApplicationController
   def create
-    @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
-    if @friendship.save
-      flash[:notice] = "Added friend."
+    if Friendship.where("(user_id = ? and friend_id = ?) or (user_id = ? and friend_id = ?)", current_user.id, params[:friend_id], params[:friend_id], current_user.id ).first
+      flash[:notice] = "Already friend, nothing to add."
       redirect_to root_url
     else
-      flash[:error] = "Unable to add friend."
-      redirect_to root_url
+      @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
+      if @friendship.save
+        flash[:notice] = "Added friend."
+        redirect_to root_url
+      else
+        flash[:error] = "Unable to add friend."
+        redirect_to root_url
+      end
     end
   end
 
