8dff9e485a2cc328a43a552c2cf31a1b10372255
Suhendra <idsuhendra@gmail.com>
2017-05-22 14:09:26 +0700

    fix debts and entries table.

--- db/migrate/20170516075438_create_debts.rb
+++ db/migrate/20170516075438_create_debts.rb
@@ -7,6 +7,7 @@ class CreateDebts < ActiveRecord::Migration[5.1]
       t.string :category
       t.date :due_date
       t.string :prove
+      t.string :status
       t.string :warning_unit
       t.integer :warning_count
       t.integer :kreditur_id, foreign_key: true
