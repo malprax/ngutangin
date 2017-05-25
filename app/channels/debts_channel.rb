3a5079580955897c9c11b6a1b0887f47fc4bc882
aulia <kingmalprax@gmail.com>
2017-05-25 09:13:15 +0800

    try to make actioncable step 2

--- app/channels/debts_channel.rb
+++ app/channels/debts_channel.rb
@@ -1,13 +1,13 @@
 class DebtsChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "blogs_#{params['debt_id']}_channel"
+    stream_from "debts_#{params['debt_id']}_channel"
   end
 
   def unsubscribed
     #code
   end
 
-  def send_comment(data)
+  def send_chat(data)
     current_user.debts.create!(message: data['chat'], chat_id: data['chat_id'])
   end
 end
