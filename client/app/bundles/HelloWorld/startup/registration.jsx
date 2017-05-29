a943131fdb5519c183ed87082ea070eb07c101ba
adhiguna utama sabril <adhiguna.sabril@gmail.com>
2017-05-29 11:17:42 +0700

    update react

--- client/app/bundles/HelloWorld/startup/registration.jsx
+++ client/app/bundles/HelloWorld/startup/registration.jsx
@@ -1,8 +1,10 @@
 import ReactOnRails from 'react-on-rails';
 
-import HelloWorld from '../components/HelloWorld';
+import HelloWorldApp from './HelloWorldApp';
+import registrationsNewApp from './registrations/new';
+import sessionNewApp from './sessions/new';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
-  HelloWorld,
+  HelloWorldApp,
 });
