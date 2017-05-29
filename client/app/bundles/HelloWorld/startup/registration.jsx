d4804ffb974e185239864edb89e1d52dc4b752df
adhiguna utama sabril <adhiguna.sabril@gmail.com>
2017-05-29 11:33:24 +0700

    update devise

--- client/app/bundles/HelloWorld/startup/registration.jsx
+++ client/app/bundles/HelloWorld/startup/registration.jsx
@@ -1,10 +1,10 @@
 import ReactOnRails from 'react-on-rails';
 
 import HelloWorldApp from './HelloWorldApp';
-import registrationsNewApp from './registrations/new';
-import sessionNewApp from './sessions/new';
+import registrationsNewApp from './devise/registrations/new';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
   HelloWorldApp,
+  registrationsNewApp
 });
