253ef446cec872b1be11323483b5ad7c59cae37b
rizki <rizki113070213@gmail.com>
2017-06-01 16:30:11 +0700

    index pages layout

--- client/app/bundles/HelloWorld/startup/registration.jsx
+++ client/app/bundles/HelloWorld/startup/registration.jsx
@@ -3,6 +3,7 @@ import ReactOnRails from 'react-on-rails';
 import HelloWorldApp from './HelloWorldApp';
 import RegistrationsNewApp from './devise/registrations/new';
 import SessionsNewApp from './devise/sessions/new';
+import IndexApp from './IndexApp';
 import TestHelloWorld from './TestHelloWorld';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
@@ -10,5 +11,6 @@ ReactOnRails.register({
   HelloWorldApp,
   RegistrationsNewApp,
   SessionsNewApp,
+  IndexApp,
   TestHelloWorld
 });
