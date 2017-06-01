00756c3d08de1977ce34c6dbf0dfcdb553186a00
rizki <rizki113070213@gmail.com>
2017-06-01 16:32:51 +0700

    login-register layout

--- client/app/bundles/HelloWorld/components/devise/registrations/new.jsx
+++ client/app/bundles/HelloWorld/components/devise/registrations/new.jsx
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
-import LayoutHeader from '../../layouts/header';
+import LayoutsHeader from '../../layouts/header';
 import {Card, CardHeader, CardText} from 'material-ui/Card';
 import Divider from 'material-ui/Divider';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
-import LayoutFooter from '../../layouts/footer';
+import LayoutsFooter from '../../layouts/footer';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 // default matererial ui
@@ -15,7 +15,7 @@ class RegistrationsNewDevise extends Component {
   render() {
     return (
       <div>
-        <LayoutHeader />
+        <LayoutsHeader />
 
           <div className="login-register">
             <Card>
@@ -25,11 +25,11 @@ class RegistrationsNewDevise extends Component {
             <Divider/>
 
               <CardText>
-                <RaisedButton label="Register with Facebook" primary={true} fullWidth={true} /><br /><br /><br /><br /><br />
-                <TextField hintText="Username" fullWidth={true} /><br />
-                <TextField hintText="Email" fullWidth={true} /><br />
-                <TextField type="password" hintText="Password" fullWidth={true} /><br />
-                <TextField type="password" hintText="Confirm Password" fullWidth={true} /><br /><br />
+                <RaisedButton label="Register with Facebook" primary={true} fullWidth={true} /><br /><br />
+                <TextField floatingLabelText="Username" fullWidth={true} />
+                <TextField floatingLabelText="Email" fullWidth={true} />
+                <TextField type="password" floatingLabelText="Password" fullWidth={true} />
+                <TextField type="password" floatingLabelText="Confirm Password" fullWidth={true} /><br /><br />
                 <RaisedButton label="Register" primary={true} fullWidth={true} />
                 <p>Already have an account?</p>
                 <p className="devise">Login now</p>
@@ -38,7 +38,7 @@ class RegistrationsNewDevise extends Component {
             </Card>
           </div>
 
-        <LayoutFooter />
+        <LayoutsFooter />
       </div>
     );
   }
